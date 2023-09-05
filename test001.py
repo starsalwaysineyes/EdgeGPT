21385975795666306949de1dc680b79516402a4f
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2023-09-05 15:46:33 +0000

    rewrite something like the prompt

--- test001.py
+++ test001.py
@@ -1,6 +1,8 @@
 import asyncio, json
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 
+from EdgeGPT.EdgeUtils import Query, Cookie
+Cookie.dir_path="/workspaces/EdgeGPT/bing_cookies"
 async def main():
     bot = await Chatbot.create() # 导入 cookie 是“可选”的，如前所述
     response = await bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative, simplify_response=True)
--- test002.py
+++ test002.py
@@ -1,5 +1,5 @@
 from EdgeGPT.EdgeUtils import Query, Cookie
 Cookie.dir_path="/workspaces/EdgeGPT/bing_cookies"
 
-q = Query("你是谁？用python代码给出回答")
+q = Query("帮我搜集和室温超导的资料")
 print(q)
